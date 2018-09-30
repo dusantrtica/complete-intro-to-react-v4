8e29cefdd44a58c17c34a4ae34fc358791991321
Dusan Trtica <dtrtica@gogoair.com>
2018-09-30 22:29:24 +0200

    Added emotion for css in js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { Router, Link } from "@reach/router";
-import pf from "petfinder-client";
-import Results from "./Results";
-import Details from "./Details";
-import SearchParams from "./SearchParams";
-import { Provider } from "./SearchContext";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Router } from '@reach/router';
+import pf from 'petfinder-client';
+import Results from './Results';
+import Details from './Details';
+import SearchParams from './SearchParams';
+import { Provider } from './SearchContext';
+import NavBar from './NavBar';
 
 /*
 API Key
@@ -20,7 +21,7 @@ Active
 
 const petfinder = pf({
   key: process.env.API_KEY,
-  secret: process.env.API_SECRET
+  secret: process.env.API_SECRET,
 });
 
 class App extends React.Component {
@@ -28,32 +29,32 @@ class App extends React.Component {
     super(props);
 
     this.state = {
-      location: "Seattle, WA",
-      animal: "",
-      breed: "",
+      location: 'Seattle, WA',
+      animal: '',
+      breed: '',
       breeds: [],
       handleAnimalChange: this.handleAnimalChange,
       handleBreedChange: this.handleBreedChange,
       handleLocationChange: this.handleLocationChange,
-      getBreeds: this.getBreeds
+      getBreeds: this.getBreeds,
     };
   }
   handleLocationChange = event => {
     this.setState({
-      location: event.target.value
+      location: event.target.value,
     });
   };
   handleAnimalChange = event => {
     this.setState(
       {
-        animal: event.target.value
+        animal: event.target.value,
       },
       this.getBreeds
     );
   };
   handleBreedChange = event => {
     this.setState({
-      breed: event.target.value
+      breed: event.target.value,
     });
   };
   getBreeds() {
@@ -67,7 +68,7 @@ class App extends React.Component {
             Array.isArray(data.petfinder.breeds.breed)
           ) {
             this.setState({
-              breeds: data.petfinder.breeds.breed
+              breeds: data.petfinder.breeds.breed,
             });
           } else {
             this.setState({ breeds: [] });
@@ -76,21 +77,14 @@ class App extends React.Component {
         .catch(console.error);
     } else {
       this.setState({
-        breeds: []
+        breeds: [],
       });
     }
   }
   render() {
     return (
       <div>
-        <header>
-          <Link to="/">Adopt Me!</Link>
-          <Link to="/search-params">
-            <span aria-label="search" role="img">
-              🔍
-            </span>
-          </Link>
-        </header>
+        <NavBar />
         <Provider value={this.state}>
           <Router>
             <Results path="/" />
@@ -103,4 +97,4 @@ class App extends React.Component {
   }
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById('root'));
